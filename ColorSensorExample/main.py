8b9e71dfc5e8cd8ed5fdf9e679794630bf8c0f0a
Bothell Robotics <33183006+BothellRobotics@users.noreply.github.com>
2020-02-09 17:48:40 -0800

    Update main.py

--- ColorSensorExample/main.py
+++ ColorSensorExample/main.py
@@ -5,8 +5,8 @@ from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
                                  InfraredSensor, UltrasonicSensor, GyroSensor)
 from pybricks.parameters import (Port, Stop, Direction, Button, Color,
                                  SoundFile, ImageFile, Align)
-from pybricks.tools import print, wait, StopWatch
 from pybricks.robotics import DriveBase
+import time
 
 # Write your program here
 brick.sound.beep()
@@ -14,11 +14,11 @@ brick.sound.beep()
 motorA = Motor(Port.A)
 motorD = Motor(Port.D)
 robot = DriveBase(motorA, motorD, 56, 114)
-cs = ColorSensor(Port.S1)
+cs = ColorSensor(Port.S4)
 
 #drive speed at 450 mm per second with 0 deg per second turn rate
 robot.drive(450, 0)
 while cs.color() != Color.YELLOW:
-     wait(50)
+     time.sleep(0.01)
 
 robot.stop(Stop.BRAKE)
